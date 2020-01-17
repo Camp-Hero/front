4bd2b1743eb57fde31b367a36a56550c2cc369ca
ImMyst <titouan.galvani@laposte.net>
2020-01-17 17:33:23 +0100

    💄 Add Vuetify

---
+++ src/components/Menu.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <span class="display-1">Camp'Hero</span>
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Menu"
+};
+</script>
