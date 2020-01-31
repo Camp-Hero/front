6c9ab171135e2a16cbdeb5ff724f8e47b7fd79b7
ImMyst <titouan.galvani@laposte.net>
2020-01-31 17:38:13 +0100

    🔥 Add login/register

---
+++ layouts/login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <section class="has-bg-img">
+      <div class="hero">
+        <nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.has-bg-img {
+  background: url('/img/background.jpeg') center center;
+  background-size: cover;
+}
+.hero {
+  height: 100vh;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/dashboard.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="section">
+    <h2 class="title is-3 has-text-grey">
+      {{ camping.title }}
+    </h2>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      camping: {
+        title: 'Camping des flots bleus'
+      }
+    }
+  }
+}
+</script>
